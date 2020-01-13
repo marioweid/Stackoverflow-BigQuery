2f09d767d52a4c0f2b5bd5e7ac451995cb4e6cf4
S4ltyGo4t <mario.weidner@gmx.de>
2020-01-13 08:39:29 +0100

    added gitignore

---
+++ index.js
@@ -0,0 +1,23 @@
+// Imports the Google Cloud client library
+const {BigQuery} = require('@google-cloud/bigquery');
+
+async function createDataset() {
+  // Creates a client
+  const bigQueryClient = new BigQuery();
+
+
+  const que = `SELECT EXTRACT(YEAR FROM creation_date) AS Year,
+    COUNT(*) AS Number_of_Questions,
+      ROUND(100 * SUM(IF(answer_count > 0, 1, 0)) / COUNT(*), 1) AS Percent_Questions_with_Answers
+      FROM
+        \`bigquery-public-data.stackoverflow.posts_questions\`
+          GROUP BY
+            Year
+          ORDER BY
+            Year`;
+  const data = await bigQueryClient.query(que);
+  console.log(data.keys());
+  // console.log(`Dataset ${dataset.id} created.`);
+}
+
+createDataset().then(ret => console.log(ret));
